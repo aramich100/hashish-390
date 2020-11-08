836cae700829d8e55822a94eaebc32312e3dc623
Michael Arabian <michael-nareg99@hotmail.com>
2020-11-08 18:20:45 -0500

    Sprint 2.

--- src/main/java/com/example/hashish/Activity/Profile.java
+++ src/main/java/com/example/hashish/Activity/Profile.java
@@ -2,22 +2,34 @@ package com.example.hashish.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
 
 import com.example.hashish.R;
 
 public class Profile extends AppCompatActivity {
-   // protected EditText name;
-   // protected EditText age;
-   // protected EditText plant;
+
+    static final UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    protected  Button onButton;
+    protected Button offButton;
     protected Button save;
     protected Button next;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +37,82 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         setUpUI();
+        setUpBluetooth();
+
+
+
+
     }
+
+
+    protected void setUpBluetooth(){
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        System.out.println("ID IS : ******" + btAdapter.getBondedDevices());
+
+        BluetoothDevice hc05 = btAdapter.getRemoteDevice("00:18:91:D7:28:92");
+        System.out.println(hc05.getName());
+
+        BluetoothSocket btSocket = null;
+
+
+            try {
+                btSocket = hc05.createRfcommSocketToServiceRecord(mUUID);
+                System.out.println(btSocket);
+                btSocket.connect();
+
+                System.out.println("Is bluetooth connected: "+ btSocket.isConnected());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+/*
+        try {
+            OutputStream outputStream = btSocket.getOutputStream();
+            outputStream.write(48);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        InputStream inputStream = null;
+        try {
+
+           // byte[] rawBytes = new byte[2];
+           // inputStream.read(rawBytes);
+           // final String temp = new String(rawBytes,"UTF-8");
+
+            inputStream = btSocket.getInputStream();
+            //inputStream.skip(inputStream.available());
+
+            for(int j=0; j<10; j++){
+                System.out.print("Temp is : ");
+            for(int i=0; i<2; i++) {
+                char temp = (char) inputStream.read();
+                System.out.print(temp);
+            }
+                System.out.println();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            btSocket.close();
+            System.out.println(btSocket.isConnected());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
     protected void setUpUI(){
 
-        //name = findViewById(R.id.nameEditText);
-        //age = findViewById(R.id.ageEditText);
-       // plant = findViewById(R.id.plantEditText);
         save = findViewById(R.id.save);
         next = findViewById(R.id.nextPage);
+        onButton = findViewById(R.id.onButton);
+        offButton = findViewById(R.id.onButton);
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,14 +124,6 @@ public class Profile extends AppCompatActivity {
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferenceHelper sharedPreferenceHelper = new SharedPreferenceHelper(Profile.this);
-                //String names = name.getText().toString();
-               // int ages = Integer.parseInt(age.getText().toString());
-              //  String plants = plant.getText().toString();
-
-               // sharedPreferenceHelper.saveAge(ages);
-               // sharedPreferenceHelper.saveProfileName(names);
-                //sharedPreferenceHelper.savePlant(plants);
 
                 Toast toast = Toast.makeText(getApplicationContext(), "SAVED", Toast.LENGTH_LONG);
                 toast.show();
@@ -71,9 +142,7 @@ public class Profile extends AppCompatActivity {
 
         super.onStart();
         SharedPreferenceHelper sharedPreferenceHelper = new SharedPreferenceHelper(Profile.this);
-       // name.setText(sharedPreferenceHelper.getProfileName());
-       // age.setText(Integer.toString(sharedPreferenceHelper.getAge()));
-       // plant.setText(sharedPreferenceHelper.getPlantName());
+
     }
 
 }
